09494392731337fdac1a964f48d9f9607d5e9faa
adrianaalvaro <124527651+adrianaalvaro@users.noreply.github.com>
2023-11-18 13:44:57 +0100

    little change

--- src/adriana.py
+++ src/adriana.py
@@ -3,18 +3,6 @@ import pandas as pd
 import os
 
 
-def get_list_countries(data_folder):
-
-    files = os.listdir(data_folder)
-    countries = []
-    for file_name in files:
-        if file_name.startswith('load'):
-            country = file_name.split('_')[1][:-4]
-            countries.append(country)
-    countries = list(set(countries))
-
-    return countries
-
 
 def get_generation_files(country, data_folder):
     files = os.listdir(data_folder)
