9782d187b9c63f31f0061e4bbe0a0e8f02ff0f8d
adrianaalvaro <124527651+adrianaalvaro@users.noreply.github.com>
2023-11-18 13:43:40 +0100

    pruebas changed

--- src/adriana.py
+++ src/adriana.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 
-def get_list_countries(data_folder='data'):
+def get_list_countries(data_folder):
 
     files = os.listdir(data_folder)
     countries = []
@@ -16,13 +16,13 @@ def get_list_countries(data_folder='data'):
     return countries
 
 
-def get_generation_files(country, data_folder = 'data'):
+def get_generation_files(country, data_folder):
     files = os.listdir(data_folder)
     gen_file_list = [file for file in files if file.startswith(f'gen_{country}')]
     return gen_file_list
 
 
-def get_load_file(country, data_folder = 'data'):
+def get_load_file(country, data_folder):
     files = os.listdir(data_folder)
     for file in files:
         if file.startswith(f'load_{country}'):
